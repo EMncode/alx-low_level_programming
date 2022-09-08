4e395bcf2aa73459496d413bcc87f021d87750ec
EMncode <emayorinde@gmail.com>
2022-09-08 15:54:16 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * return:Always 0 (success)
+ */
 
 int main()
 {
-	puts(*\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 
 }

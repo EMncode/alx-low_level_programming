590f6878c8ec76f371e0c860b598e6811b3b31c0
EMncode <emayorinde@gmail.com>
2022-09-09 20:02:04 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -14,9 +14,9 @@ int main(void)
 
 {
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = '0' ; ch <= '9' ; ch++)
 putchar(ch);
-for (ch = 'A' ; ch <= 'Z' ; ch++)
+for (ch = '0' ; ch <= '9' ; ch++)
 putchar(ch);
 putchar('\n');
 return (0);

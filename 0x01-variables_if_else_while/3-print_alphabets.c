0cfbf244c54f9296dfb9abf80926fc6949df70d0
EMncode <emayorinde@gmail.com>
2022-09-09 20:29:24 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,16 +15,11 @@
 int main(void)
 
 {
-int n;
-int l;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-else if (l == 0)
-printf("Last digit of %d is %d and is 0\n", n, l);
-else
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+putchar(ch);
+putchar('\n');
 return (0);
 }

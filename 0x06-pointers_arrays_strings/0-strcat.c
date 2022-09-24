be4f9f58412a85332895d02c6787f4a0d735083d
EMncode <emayorinde@gmail.com>
2022-09-23 20:06:01 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,24 +16,18 @@ char *_strcat(char *dest, char *src)
 
 {
 
+int index = 0, dest_len = 0;
 
+while (dest[index++])
 
-		int index = 0, dest_len = 0;
+	dest_len++;
 
+for (index = 0; src[index]; index++)
 
-
-			while (dest[index++])
-
-						dest_len++;
-
-
-
-				for (index = 0; src[index]; index++)
-
-							dest[dest_len++] = src[index];
+dest[dest_len++] = src[index];
 
 
 
-					return (dest);
+return (dest);
 
 }

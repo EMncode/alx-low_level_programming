0109820a30c2515a69ec5790186171043ec1028b
EMncode <emayorinde@gmail.com>
2022-09-24 15:50:15 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
-int main(void)
-{
-	char *sh = "_putchar";
+/**
+*main - Entry point
+*
+*Return: 0 nothing, return void
+*/
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
-
 }
 
